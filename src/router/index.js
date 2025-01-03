05ccce68c9b82b07963267203b49beefa9b5a42d
Njb <sdr.expert@gmail.com>
2025-01-03 12:15:28 +0200

    done build with token

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TaskManagerView from "../views/TaskManagerView.vue";
 
-const routes = [{ path: "/", name: "home", component: TaskManagerView }];
+const routes = [
+  { path: "/", name: "home", component: TaskManagerView },
+  {
+    path: "/alls-task",
+    name: "alls-task",
+    component: () => import("../components/ui/CardSearch.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
