2d6f91c095a8358225bc4c15cb33bbb40e4fd399
Njb <sdr.expert@gmail.com>
2025-01-04 22:19:04 +0200

    added card with actuals data-ok

--- src/components/ui/CardSearch.vue
+++ src/components/ui/CardSearch.vue
@@ -1,5 +1,5 @@
 <template>
-    <div
+    <!-- <div
       class="task-card p-4 border rounded-lg shadow hover:shadow-lg cursor-pointer"
       :class="{ 'bg-blue-50': isSelected }"
       @click="$emit('click')"
@@ -17,7 +17,39 @@
       >
         Подробнее
       </a>
+    </div> -->
+
+<div class="card-search cursor-pointer shadow-lg relative flex flex-col bg-white border border-slate-200 rounded-lg mx-auto w-[98%] md:w-[375px] overflow-hidden hover:scale-105 transition duration-300 ease" v-auto-animate
+@click="$emit('click')"
+>      
+    <div class="mx-3 mb-0 border-b border-slate-200 pt-3 pb-2 px-1">
+        <span class="text-sm font-medium text-slate-600">
+        Status: <span class="text-green-500">Ok</span>
+        </span>
     </div>
+    
+    <div class="p-4">
+        <h5 class="mb-2 text-slate-800 text-xl font-semibold overflow-hidden">
+            {{ task.name }}
+        </h5>
+        <p class="text-slate-600 leading-normal font-light">
+            {{ task.content }}
+        </p>
+    </div>
+    <div class="mx-3 border-t border-slate-200 pb-3 pt-2 px-1 flex justify-between">
+        <span class="text-sm text-slate-600 font-medium">
+        {{ task.created_at.slice(0, 5)}}
+        </span>
+
+        <a href="#" class="text-slate-800 font-semibold text-sm hover:underline flex items-center">
+        Learn More
+            <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4 svg-arrow-r " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+            </svg>
+        </a>
+    </div>
+</div>
+
 </template>
   
 <script setup>
