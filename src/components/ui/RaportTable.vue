ff4737f8d85791ae3d62fc971e28b6b6127e1b82
Njb <sdr.expert@gmail.com>
2025-01-09 15:17:35 +0200

    all is done4

--- src/components/ui/RaportTable.vue
+++ src/components/ui/RaportTable.vue
@@ -71,23 +71,25 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><mask id="lineMdCloseCircleFilled0"><g fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path fill="#fff" fill-opacity="0" stroke-dasharray="64" stroke-dashoffset="64" d="M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9Z"><animate fill="freeze" attributeName="fill-opacity" begin="0.6s" dur="0.5s" values="0;1"/><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="64;0"/></path><path stroke="#000" stroke-dasharray="8" stroke-dashoffset="8" d="M12 12l4 4M12 12l-4 -4M12 12l-4 4M12 12l4 -4"><animate fill="freeze" attributeName="stroke-dashoffset" begin="1.1s" dur="0.2s" values="8;0"/></path></g></mask><rect width="24" height="24" fill="#000" mask="url(#lineMdCloseCircleFilled0)"/></svg>
       </div>
       <div class="logs-body">
-        <h3 class="text-center font-bold text-slate-400">Логи задачи {{ selectedTaskId }}</h3>
-      <table class="w-full text-left table-auto min-w-max text-slate-800">
-        <thead>
-          <tr class="bg-slate-300 rounded-xl">
-            <th class="p-4">Статус</th>
-            <th class="p-4">Сообщение</th>
-            <th class="p-4">Дата</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="log in formStore.taskLogs[selectedTaskId]" :key="log.id">
-            <td class="p-4">{{ log.status }}</td>
-            <td class="p-4">{{ log.message }}</td>
-            <td class="p-4">{{ new Date(log.created_at).toLocaleString() }}</td>
-          </tr>
-        </tbody>
-      </table>
+        <div class="logs-table">
+            <h3 class="text-center font-bold text-slate-400">Логи задачи {{ selectedTaskId }}</h3>
+            <table class="w-full text-left table-auto min-w-max text-slate-800">
+              <thead>
+                <tr class="bg-slate-300 rounded-xl">
+                  <th class="p-4">Статус</th>
+                  <th class="p-4">Сообщение</th>
+                  <th class="p-4">Дата</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="log in formStore.taskLogs[selectedTaskId]" :key="log.id">
+                  <td class="p-4">{{ log.status }}</td>
+                  <td class="p-4">{{ log.message }}</td>
+                  <td class="p-4">{{ new Date(log.created_at).toLocaleString() }}</td>
+                </tr>
+              </tbody>
+            </table>
+         </div>
       </div>
     </div>
   </div>
@@ -133,16 +135,20 @@ const closeLogs = () => {
 }
 .logs-body{
 	width: 100%;
-	height: 70%;
+	height: 80%;
 	background: rgb(241 245 249);
   overflow: hidden;
 	padding: 20px;
 	z-index: 100;
 	border-radius: 20px;
-  overflow-x: scroll;
-  overflow-x: auto;
   padding-top: 55px;
   
+}
+.logs-table{
+  width: 100%;
+  height: 100%;
+  overflow-x: scroll;
+  overflow-x: auto;
 }
 .logs-body h3{
     position: fixed;
