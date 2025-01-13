42eaac6a80e36af13b3f06b670e7dd30faa6ba23
Njb <sdr.expert@gmail.com>
2025-01-13 09:05:40 +0200

    added new ngrok

--- src/consts/const.js
+++ src/consts/const.js
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = "http://localhost:3082/api";
-export const API_BASE_URL = "https://873c-176-37-193-72.ngrok-free.app/api";
+export const API_BASE_URL = "https://f868-176-37-193-72.ngrok-free.app/api";
