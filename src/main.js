44cc4689cdfca793026de3373fc8da48ad05c5f7
Njb <sdr.expert@gmail.com>
2025-03-24 11:42:21 +0200

    added table to single page9

--- src/main.js
+++ src/main.js
@@ -13,24 +13,25 @@ app.use(autoAnimatePlugin);
 app.use(pinia);
 app.use(VueTelegramPlugin);
 
+console.log("VueTelegramPlugin доступен:", VueTelegramPlugin);
 console.log("Telegram API доступен:", window.Telegram);
 console.log("Telegram WebApp доступен:", window.Telegram?.WebApp);
 
-const telegram = window.Telegram?.WebApp;
+// const telegram = window.Telegram?.WebApp;
 
-if (telegram) {
-  telegram.expand();
-  telegram.ready();
-  console.log("Telegram WebApp инициализировано");
+// if (telegram) {
+//   telegram.expand();
+//   telegram.ready();
+//   console.log("Telegram WebApp инициализировано");
 
-  const initData = telegram.initData;
-  console.log("Получено initData:", initData);
+//   const initData = telegram.initData;
+//   console.log("Получено initData:", initData);
 
-  if (initData) {
-    console.warn("Telegram WebApp API initData не требуется в этом варианте.");
-  }
-} else {
-  console.warn("Telegram WebApp API не доступен.");
-}
+//   if (initData) {
+//     console.warn("Telegram WebApp API initData не требуется в этом варианте.");
+//   }
+// } else {
+//   console.warn("Telegram WebApp API не доступен.");
+// }
 
 app.mount("#app");
